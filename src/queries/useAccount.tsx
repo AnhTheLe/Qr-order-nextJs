46f0c78d8e183a7fba8128d0e8fe1fbace3f7365
anh the le <theanh19082002@gmail.com>
2025-03-11 20:22:40 +0700

    change password and update profile

--- src/queries/useAccount.tsx
+++ src/queries/useAccount.tsx
@@ -1,9 +1,25 @@
 import accountApiRequest from "@/apiRequests/account"
-import { useQuery } from "@tanstack/react-query"
+import { AccountResType } from "@/schemaValidations/account.schema"
+import { useMutation, useQuery } from "@tanstack/react-query"
 
-export const useAccountProfile = () => {
+export const useAccountMe = (onSuccess?: (data: AccountResType) => void) => {
     return useQuery({
-        queryKey: ["account-profile"],
-        queryFn: accountApiRequest.me,
+        queryKey: ["account-me"],
+        queryFn: () => accountApiRequest.me().then(res => {
+            if (onSuccess) onSuccess(res.payload)
+            return res
+        }),
+    })
+}
+
+export const useUpdateMeMutation = () => {
+    return useMutation({
+        mutationFn: accountApiRequest.updateMe,
+    })
+} 
+
+export const useChangePasswordMutation = () => {
+    return useMutation({
+        mutationFn: accountApiRequest.changePassword,
     })
 }
